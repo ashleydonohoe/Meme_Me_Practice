2ddd2a3f5d10ef28a5c1fa54bfeb5075a549dada
Ashley Donohoe <ashleyrdonohoe@gmail.com>
2016-03-20 17:05:49 -0400

    testing hiding and showing the keyboard

--- MemeMePractice/ViewController.swift
+++ MemeMePractice/ViewController.swift
@@ -19,19 +19,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var bottomText: UITextField!
     
     
+    // Dictionary to hold text attributes
     let memeTextAttributes = [
         NSStrokeColorAttributeName: UIColor.blackColor(),
         NSForegroundColorAttributeName: UIColor.whiteColor(),
         NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
-        NSStrokeWidthAttributeName: 4.0
+        NSStrokeWidthAttributeName: -4.0
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        topText.text = "TOP"
-        bottomText.text = "BOTTOM"
         
+        // Assigns placeholder text that will disappear when user starts typing their meme text
+        topText.attributedPlaceholder = NSAttributedString(string: "TOP", attributes:  memeTextAttributes)
+        bottomText.attributedPlaceholder = NSAttributedString(string: "BOTTOM", attributes: memeTextAttributes)
+        
+        // Sets default text attributes and centers text
         topText.defaultTextAttributes = memeTextAttributes
         bottomText.defaultTextAttributes = memeTextAttributes
         
@@ -40,7 +43,14 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     
     override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
+        self.subscribeToKeyboardNotifications()
+    }
+    
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.unsubscribeFromKeyboardNotifications()
     }
     
     // Presents Image Picker to user
@@ -69,25 +79,37 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     // Allows user to share meme to social media, send via email, etc
     @IBAction func shareMeme(sender: UIBarButtonItem) {
-        let image = imagePickerView.image
-        let controller = UIActivityViewController(activityItems: [image!], applicationActivities: nil)
-        self.presentViewController(controller, animated: true, completion: nil)
+        if let image = imagePickerView.image {
+            let controller = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+            self.presentViewController(controller, animated: true, completion: nil)
+        }
     }
     
-    
-    @IBAction func userStartedEditing(sender: UITextField) {
-        topText.text = ""
-        bottomText.text = ""
+    // Moves the view when keyboard covers text field
+    func keyboardWillShow(notification: NSNotification) {
+        view.frame.origin.y -= getKeyboardHeight(notification)
     }
     
-    // Clears default text begins editing text field
-    func textFieldDidBeginEditing(textField: UITextField) {
-            topText.text = ""
-            bottomText.text = ""
+    func getKeyboardHeight(notification: NSNotification) -> CGFloat {
+        let userInfo = notification.userInfo
+        let keyboardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue // of CGRect
+        return keyboardSize.CGRectValue().height
     }
     
-    // TODO: When a user presses return, the keyboard should be dismissed. This can be accomplished in textFieldShouldReturn.
+    func subscribeToKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+    }
     
+    func unsubscribeFromKeyboardNotifications() {
+        NSNotificationCenter.defaultCenter().removeObserver(self, name:
+            UIKeyboardWillShowNotification, object: nil)
+    }
+    
+        // TODO: When a user presses return, the keyboard should be dismissed. This can be accomplished in textFieldShouldReturn.
+    /*func textFieldShouldReturn(textField: UITextField) -> Bool {
+        print("Should return")
+    }
+    */
     
 }
 
